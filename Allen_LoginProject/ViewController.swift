3e4e1cdedb549fd1a0b1697310a5071182f00884
greenbsk <qhqh147@gmail.com>
2023-02-07 00:00:23 +0900

    Initial Commit

--- Allen_LoginProject/ViewController.swift
+++ Allen_LoginProject/ViewController.swift
@@ -9,8 +9,34 @@ extension UIView {
 
 class ViewController: UIViewController {
     
-    let emailTextFieldView = UIView()
-    let mainLable = UILabel()
+    // - 이메일 입력하는 텍스트 뷰
+    private lazy var emailTextFieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.darkGray
+        view.roundCorners(radius: 5)
+        return view
+    }()
+    
+    private lazy var emailInfoLable: UILabel = {
+        let label = UILabel()
+        label.text = "이메일주소 또는 전화번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor.lightGray
+        return label
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+    let tf = UITextField()
+    tf.frame.size.height = 48
+    tf.backgroundColor = .clear
+    tf.textColor = .white
+    tf.tintColor = .white
+    tf.autocapitalizationType = .none
+    tf.autocorrectionType = .no
+    tf.spellCheckingType = .no
+    tf.keyboardType = .emailAddress
+    return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,14 +55,13 @@ class ViewController: UIViewController {
         emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
         emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
-        
-        mainLable.backgroundColor = UIColor.lightGray
-        view.addSubview(mainLable)
-        mainLable.translatesAutoresizingMaskIntoConstraints = false
-        mainLable.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 10).isActive = true
-        mainLable.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: -10).isActive = true
-        mainLable.topAnchor.constraint(equalTo: emailTextFieldView.topAnchor, constant: 10).isActive = true
-        mainLable.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        view.addSubview(emailInfoLable)
+        emailInfoLable.translatesAutoresizingMaskIntoConstraints = false
+        emailInfoLable.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 10).isActive = true
+        emailInfoLable.trailingAnchor.constraint(equalTo: emailTextFieldView.trailingAnchor, constant: -10).isActive = true
+        emailInfoLable.topAnchor.constraint(equalTo: emailTextFieldView.topAnchor, constant: 10).isActive = true
+        emailInfoLable.heightAnchor.constraint(equalToConstant: 20).isActive = true
     }
 }
+
 
